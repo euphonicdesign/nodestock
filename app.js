e21daaa5e41c4abec4d112f822ea46d2eed68a97
Sorin <zece.delfini@outlook.com>
2023-02-20 23:07:19 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+//Set handlebars middleware
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const otherstuff = "hello there, this is other stuff!";
+
+//Set handlebar routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        stuff: otherstuff
+    });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+})
